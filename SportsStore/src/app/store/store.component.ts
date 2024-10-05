ba0a936004c74404ecc3571d52826a32035d86bb
putzoludaniele3 <daniele.putzolu.0103@gmail.com>
2024-10-05 17:13:45 +0200

    adding pag

--- SportsStore/src/app/store/store.component.ts
+++ SportsStore/src/app/store/store.component.ts
@@ -15,8 +15,9 @@ export class StoreComponent{
     constructor(private repository: ProductRepository){}
 
     get products(): Product[]{
-        
-        return this.repository.getProducts(this.selectedCategory);
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+        return this.repository.getProducts(this.selectedCategory)
+            .slice(pageIndex, pageIndex + this.productsPerPage)
     }
 
     get categories(): string[] {
@@ -25,5 +26,20 @@ export class StoreComponent{
 
     changeCategory(newCategory?: string){
         this.selectedCategory = newCategory;
+    }
+
+    changePage(newPage : number){
+        this.selectedPage = newPage;
+    }
+
+    changePageSize(newSize: number){
+        this.productsPerPage= Number(newSize);
+        this.changePage(1);
+    }
+
+    get pageNumbers(): number[]{
+        return Array(Math.ceil(this.repository
+            .getProducts(this.selectedCategory).length / this.productsPerPage))
+                .fill(0).map((x, i) => i + 1);
     }
 }
