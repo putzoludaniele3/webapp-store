7bfbdd1580b3b5b32934850857fd4bbb65441aa7
putzoludaniele3 <git config --daniele.putzolu.0103@gmail.com>
2024-10-08 15:05:39 +0200

    adding routing and guard

--- SportsStore/src/app/app.module.ts
+++ SportsStore/src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StoreModule } from "./store/store.module";
+import { RouterModule } from '@angular/router';
+import { CartDetailComponent } from './store/cartDetail.component';
+import { CheckoutComponent } from './store/checkout.component';
+import { StoreComponent } from './store/store.component';
+import { StoreFirstGuard } from './storeFirst.guard';
 
 @NgModule({
   declarations: [
@@ -9,9 +14,24 @@ import { StoreModule } from "./store/store.module";
   ],
   imports: [
     BrowserModule,
-    StoreModule
+    StoreModule,
+    RouterModule.forRoot([
+      { 
+        path : "store", component: StoreComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      { path : "cart", component: CartDetailComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      { path : "checkout", component: CheckoutComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      { path : "**", redirectTo: "/store"
+
+      }
+    ])
 ],
-  providers: [],
+  providers: [StoreFirstGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
