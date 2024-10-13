6b7b75cb7880486879dfc95d443484d7a7e93b3c
Daniele Putzolu <putzoludaniele3@gmail.com>
2024-10-13 12:37:48 +0200

    fix admin auth

--- SportsStore/src/app/app.module.ts
+++ SportsStore/src/app/app.module.ts
@@ -16,21 +16,30 @@ import { StoreFirstGuard } from './storeFirst.guard';
     BrowserModule,
     StoreModule,
     RouterModule.forRoot([
-      { 
-        path : "store", component: StoreComponent,
+      {
+        path: "store", component: StoreComponent,
         canActivate: [StoreFirstGuard]
       },
-      { path : "cart", component: CartDetailComponent,
+      {
+        path: "cart", component: CartDetailComponent,
         canActivate: [StoreFirstGuard]
       },
-      { path : "checkout", component: CheckoutComponent,
+      {
+        path: "checkout", component: CheckoutComponent,
         canActivate: [StoreFirstGuard]
       },
-      { path : "**", redirectTo: "/store"
+      {
+        path: "admin",
+        loadChildren: () => import("./admin/admin.module")
+          .then(m => m.AdminModule),
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "**", redirectTo: "/store"
 
       }
     ])
-],
+  ],
   providers: [StoreFirstGuard],
   bootstrap: [AppComponent]
 })
